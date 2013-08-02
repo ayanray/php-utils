a93f18a9f6d73bf85170f0b9787f5800810c3060
Ayan Ray <ayan@ayanray.com>
2013-08-02 10:24:57 -0700

    test

---
+++ src/StringUtils.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AyanRay;
+
+class StringUtils
+{
+    public static function removeFromBeginning($needle, $haystack) {
+        $to_return = $haystack;
+        $needle_len = strlen($needle);
+        if (substr($haystack, 0, $needle_len) === $needle) {
+            $to_return = substr($haystack, $needle_len, strlen($haystack));
+        }
+
+        return $to_return;
+    }
+    
+    public static function removeFromEnd($needle, $haystack) {
+        $to_return = $haystack;
+        $needle_len = strlen($needle);
+        if (substr($haystack, -$needle_len) === $needle) {
+            $to_return = substr($haystack, 0, -$needle_len);
+        }
+
+        return $to_return;
+    }
+
+
+
+}
