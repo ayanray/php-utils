13d7307e72008358b136ba725e70af7774d7face
Ayan Ray <ayan@ayanray.com>
2013-08-02 16:57:11 -0700

    string

--- src/AyanRay/String.php
+++ src/AyanRay/String.php
@@ -6,41 +6,51 @@ class String
 {
     private $_str;
 
-    public function __construct($str) {
+    public function __construct($str)
+    {
         $this->_str = $str;
     }
 
-    public static function c($str) {
+    public static function c($str)
+    {
         return new String($str);
     }
 
-    public function getString() {
+    public function getString()
+    {
         return $this->_str;
     }
 
-    public function __toString() {
+    public function __toString()
+    {
         return $this->_str;
     }
 
-    public function addFront($add) {
+    public function addFront($add)
+    {
         if (substr($this->_str, 0, strlen($add)) !== $add) {
-            $this->_str = $add.$this->_str;
+            $this->_str = $add . $this->_str;
         }
+
         return $this;
     }
 
-    public function addEnd($add) {
+    public function addEnd($add)
+    {
         if (substr(strrev($this->_str), 0, strlen($add)) !== $add) {
             $this->_str = $this->_str . $add;
         }
+
         return $this;
     }
 
-    public function removeFront($remove) {
+    public function removeFront($remove)
+    {
         $remove_len = strlen($remove);
         if (substr($this->_str, 0, $remove_len) === $remove) {
             $this->_str = substr($this->_str, $remove_len, strlen($this->_str));
         }
+
         return $this;
     }
 
@@ -50,6 +60,35 @@ class String
         if (substr($this->_str, -$remove_len) === $remove) {
             $this->_str = substr($this->_str, 0, -$remove_len);
         }
+
+        return $this;
+    }
+
+    public function toLower()
+    {
+        $this->_str = strtolower($this->_str);
+        return $this;
+    }
+
+    public function toUpper()
+    {
+        $this->_str = strtoupper($this->_str);
+        return $this;
+    }
+
+    public function stripTags()
+    {
+        $this->_str = strip_tags($this->_str);
+
+        return $this;
+    }
+
+    public function truncate($len = 250, $add_to_end = "...")
+    {
+        if (strlen($this->_str) > $len) {
+            $this->_str = substr($this->_str, 0, $len) . $add_to_end;
+        }
+
         return $this;
     }
 }
