3466d2a37705aa332f3724e81e8c1bfa59038ff6
Ayan Ray <ayan@ayanray.com>
2013-08-02 17:12:08 -0700

    comments and added shortform trunc

--- src/AyanRay/String.php
+++ src/AyanRay/String.php
@@ -76,13 +76,39 @@ class String
         return $this;
     }
 
-    public function stripTags()
+    /**
+     * PHP Strip tags
+     * @return String
+     */
+    public function stripTags($allowable_tags = null)
     {
-        $this->_str = strip_tags($this->_str);
-
+        $this->_str = strip_tags($this->_str, $allowable_tags);
         return $this;
     }
 
+    /**
+     * Shorter Alias for truncate
+     *
+     * @param int    $len
+     * @param string $add_to_end
+     *
+     * @return String
+     */
+    public function trunc($len = 250, $add_to_end = "...")
+    {
+        $this->truncate($len, $add_to_end);
+        return $this;
+    }
+
+    /**
+     * Shrink string down to X characters and IF it's shrinked, add a string to the end
+     * I.e. I'm talking for a long time -> I'm talking for a long...
+     *
+     * @param int    $len
+     * @param string $add_to_end
+     *
+     * @return String
+     */
     public function truncate($len = 250, $add_to_end = "...")
     {
         if (strlen($this->_str) > $len) {
